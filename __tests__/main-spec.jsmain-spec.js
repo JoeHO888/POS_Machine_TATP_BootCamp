9c6ea1a83a78bc836e43be3c36f9d86c85738a3f
HOJO3 <joe.ho@oocl.com>
2018-11-21 23:23:11 +0800

    add print one line text function

--- __tests__/main-spec.jsmain-spec.js
+++ __tests__/main-spec.jsmain-spec.js
@@ -1,5 +1,37 @@
 const main = require('../main');
 
-it ('should add two numbers', () => {
-    expect(main.getAllDetailsOfAnItem(2, 3)).toBe(5);
+
+const text1 = {
+		'barcode': 'ITEM000001',
+		'name': 'Sprite',
+		'unit': 'bottle',
+		'price': 3.00,
+		'PromotionType':'BUY_TWO_GET_ONE_FREE',
+		'needToBeWeighted':false,
+		'count':1
+		
+		
+	}
+	
+const text2 = {
+		'barcode': 'ITEM000003',
+		'name': 'Litchi',
+		'unit': 'kg',
+		'price': 15.00,
+		'PromotionType':'',
+		'needToBeWeighted':true,
+		'count':2
+		
+		
+	}
+	
+	
+
+it ('should output an obeject with all details', () => {
+    expect(main.getAllDetailsOfAnItem('ITEM000001')).toEqual(text1);
 });
+
+it ('should output an obeject with all details', () => {
+    expect(main.getAllDetailsOfAnItem('ITEM000003-2')).toEqual(text2);
+});
+	
