7073af023688d11beb3a2caeb786db5ba3948eb3
HOJO3 <joe.ho@oocl.com>
2018-11-21 23:44:20 +0800

    test case for summary

--- __tests__/main-spec.jsmain-spec.js
+++ __tests__/main-spec.jsmain-spec.js
@@ -6,11 +6,9 @@ const text1 = {
 		'name': 'Sprite',
 		'unit': 'bottle',
 		'price': 3.00,
-		'PromotionType':'BUY_TWO_GET_ONE_FREE',
+		'promotionType':'BUY_TWO_GET_ONE_FREE',
 		'needToBeWeighted':false,
-		'count':1
-		
-		
+		'count':1	
 	}
 	
 const text2 = {
@@ -18,13 +16,36 @@ const text2 = {
 		'name': 'Litchi',
 		'unit': 'kg',
 		'price': 15.00,
-		'PromotionType':'',
+		'promotionType':'',
 		'needToBeWeighted':true,
 		'count':2
-		
-		
 	}
 	
+const text3 = {
+		'name': 'Litchi',
+		'unit': 'kg',
+		'price': 15.00,
+		'count':2,
+		'subtotal':30.00
+	}
+
+const input3 = [{
+		'barcode': 'ITEM000003',
+		'name': 'Litchi',
+		'unit': 'kg',
+		'price': 15.00,
+		'promotionType':'',
+		'needToBeWeighted':true,
+		'count':2
+	},{
+		'barcode': 'ITEM000001',
+		'name': 'Sprite',
+		'unit': 'bottle',
+		'price': 3.00,
+		'promotionType':'BUY_TWO_GET_ONE_FREE',
+		'needToBeWeighted':false,
+		'count':1	
+	}]
 	
 
 it ('should output an obeject with all details', () => {
@@ -33,5 +54,9 @@ it ('should output an obeject with all details', () => {
 
 it ('should output an obeject with all details', () => {
     expect(main.getAllDetailsOfAnItem('ITEM000003-2')).toEqual(text2);
+});
+
+it ('should output a summary for a specific barcode', () => {
+    expect(main.getSummary(input3,'ITEM000003')).toEqual(text3);
 });
 	
